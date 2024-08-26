7704f9df020a330391b04a68d0e2a75037d40540
muhammed-jasir <jasirmm307@gmail.com>
2024-08-26 14:10:00 +0530

    react router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup'
 
 const App = () => {
     return (
         <div>
-            <Home />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/signup' element={<Signup />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
